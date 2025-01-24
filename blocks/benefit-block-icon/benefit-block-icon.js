35f0abc47cd7443d2b69e3c5764c26e0b28e2718
Bhargava <2363065@cognizant.com>
2025-01-24 11:43:27 +0530

    code merge 24 Jan 2025

--- blocks/benefit-block-icon/benefit-block-icon.js
+++ blocks/benefit-block-icon/benefit-block-icon.js
@@ -5,25 +5,30 @@ export default function decorate(block) {
   blockleft.classList.add('block-left');
   block.prepend(blockleft);
   blockleft.append(innerleftDivs[0]);
+
   // slicing remaining elements of the block
   const childLength = [...block.children].length;
   const innerrightDivs = [...block.children].slice(1, childLength);
   const blockRight = document.createElement('div');
   blockRight.classList.add('block-right');
   block.append(blockRight);
+
   if (innerrightDivs) {
     blockRight.append(...innerrightDivs);
-    // Change all the second <h2> to <h4>
+
+    // Change all the <h2> to <h4> if it has only text content
     const divs = blockRight.querySelectorAll('div');
     divs.forEach((div) => {
       const h2s = div.querySelectorAll('h2');
-      if (h2s.length > 1) {
-        const secondH2 = h2s[1];
-        const h4 = document.createElement('h4');
-        h4.innerHTML = secondH2.innerHTML; // Copy the inner HTML
-        secondH2.replaceWith(h4); // Replace the <h2> with <h4>
-      }
+      h2s.forEach((h2) => {
+        if (h2.childNodes.length >= 1 && h2.textContent !== '') {
+          const h4 = document.createElement('h4');
+          h4.innerHTML = h2.innerHTML; // Copy the inner HTML
+          h2.replaceWith(h4); // Replace the <h2> with <h4>
+        }
+      });
     });
+
     // Remove empty divs
     const emptyDivs = blockRight.querySelectorAll('div:empty');
     emptyDivs.forEach((div) => div.remove());
--- blocks/marketo-form/marketo-form.js
+++ blocks/marketo-form/marketo-form.js
@@ -1,11 +1,30 @@
-function addScript(src) {
+// function addScript(src) {
+//   const script = document.createElement('script');
+//   script.src = src;
+//   document.head.appendChild(script);
+// }
+// (() => {
+//   // addScript('//899-BTB-436.mktoweb.com/js/forms2/js/forms2.min.js');
+//   addScript('//pages.wellmark.com/js/forms2/js/forms2.min.js');
+// })();
+
+function addScript(src, id) {
   const script = document.createElement('script');
   script.src = src;
+  if (id) {
+    script.id = id;
+  }
   document.head.appendChild(script);
 }
+
 (() => {
   addScript('//899-BTB-436.mktoweb.com/js/forms2/js/forms2.min.js');
+  //  new line
+  addScript('//pages.wellmark.com/js/forms2/js/forms2.min.js');
+  addScript('https://899-BTB-436.mktoweb.com/rs/899-BTB-436/images/teknkl-formsplus-core-1.0.8.js?version=0', 'teknkl-FormsPlus-Core-1.x');
+  addScript('https://899-BTB-436.mktoweb.com/rs/899-BTB-436/images/teknkl-simpledto-2.0.4.js?version=0', 'teknkl-SimpleDTO-2.x');
 })();
+
 // creation of block for marketo form
 export default function decorate(block) {
   [...block.children].forEach((row) => {
@@ -19,6 +38,8 @@ export default function decorate(block) {
     setTimeout(() => {
       const formId = parseInt(idstringValue, 10);
       window.MktoForms2.loadForm('//899-BTB-436.mktoweb.com', '899-BTB-436', formId);
+      //  new line
+      window.MktoForms2.loadForm('//pages.wellmark.com', '464-UUV-172', formId);
     }, 1000);
   });
 }
