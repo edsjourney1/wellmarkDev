8087035f11c9174bd1ed1bbf677a8892ecd55c04
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2025-01-26 08:38:25 +0530

    article-listing-description

--- blocks/blog-article-listing-description/blog-article-listing-description.js
+++ blocks/blog-article-listing-description/blog-article-listing-description.js
@@ -157,4 +157,155 @@ export default async function decorate(block) {
     blockDiv.append(mainDiv);
   });
   block.append(headDiv, blockDiv);
+  const paginationDiv = document.createElement('div');
+  paginationDiv.classList.add('pagination');
+  const paginationContainer = document.createElement('div');
+  paginationContainer.classList.add('pagination-container');
+  const countList = document.createElement('div');
+  countList.classList.add('count-list');
+  const countResult = document.createElement('div');
+  countResult.classList.add('count-result');
+  paginationContainer.append(countList, countResult);
+  paginationDiv.append(paginationContainer);
+  block.append(headDiv, blockDiv, paginationDiv);
+
+  const itemsPerPage = 9;
+  let currentPage = 1;
+  function renderItems() {
+    blockDiv.innerHTML = '';
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const jsonVar = myJson.slice(startIndex, endIndex);
+    jsonVar.forEach(({
+      ImageUrl, category, publishedDate, articleReadTime, title,
+    }) => {
+      const mainDiv = document.createElement('div');
+      mainDiv.classList.add('card-div');
+
+      const image = document.createElement('img');
+      image.src = ImageUrl;
+      image.alt = 'thumbnail';
+      mainDiv.appendChild(image);
+
+      const contentDiv = document.createElement('div');
+      contentDiv.classList.add('content-div');
+      mainDiv.appendChild(contentDiv);
+
+      const mainTitle = document.createElement('h3');
+      mainTitle.classList.add('card-title');
+      mainTitle.textContent = title;
+      contentDiv.appendChild(mainTitle);
+
+      const datetimeDiv = document.createElement('div');
+      datetimeDiv.classList.add('date-div');
+      contentDiv.appendChild(datetimeDiv);
+
+      const pubDate = document.createElement('p');
+      pubDate.classList.add('date');
+      pubDate.textContent = publishedDate;
+      datetimeDiv.appendChild(pubDate);
+
+      const arcretime = document.createElement('p');
+      arcretime.classList.add('read-time');
+      arcretime.textContent = articleReadTime;
+      datetimeDiv.appendChild(arcretime);
+
+      const categoryPara = document.createElement('p');
+      categoryPara.classList.add('category-list');
+      contentDiv.appendChild(categoryPara);
+
+      category.forEach((item) => {
+        const anchor = document.createElement('a');
+        anchor.href = 'www.google.com';
+        anchor.textContent = item;
+        categoryPara.appendChild(anchor);
+      });
+
+      blockDiv.appendChild(mainDiv);
+    });
+    countList.innerHTML = '';
+    const showingText = document.createElement('p');
+    showingText.textContent = 'Showing';
+    countList.appendChild(showingText);
+    const showingSpan = document.createElement('span');
+    showingSpan.textContent = `${startIndex + 1} - ${Math.min(endIndex, myJson.length)}`;
+    countList.appendChild(showingSpan);
+    const ofText = document.createElement('p');
+    ofText.textContent = 'of';
+    countList.appendChild(ofText);
+    const ofSpan = document.createElement('span');
+    ofSpan.textContent = myJson.length;
+    countList.appendChild(ofSpan);
+  }
+
+  function renderPagination() {
+    const totalPages = Math.ceil(myJson.length / itemsPerPage);
+    countResult.innerHTML = '';
+    function newFunction(i) {
+      currentPage = i;
+      renderItems();
+    }
+
+    // Always show the first three pages
+    for (let i = 1; i <= Math.min(3, totalPages); i += 1) {
+      const button = document.createElement('button');
+      button.textContent = i;
+      button.addEventListener('click', () => {
+        newFunction(i);
+      });
+      countResult.appendChild(button);
+    }
+    // Add ellipsis if there are more than 3 pages
+    if (totalPages > 3) {
+      const ellipsis = document.createElement('button');
+      ellipsis.textContent = '...';
+      countResult.appendChild(ellipsis);
+    }
+
+    // Always show the last page
+    if (totalPages > 3) {
+      const lastButton = document.createElement('button');
+      lastButton.textContent = totalPages;
+      lastButton.addEventListener('click', () => {
+        currentPage = totalPages;
+        renderItems();
+      });
+      countResult.appendChild(lastButton);
+    }
+    const firstPageButton = document.createElement('p');
+    firstPageButton.innerHTML = '<i class="fa-regular fa-chevrons-left"></i>';
+    firstPageButton.addEventListener('click', () => {
+      currentPage = 1;
+      renderItems();
+    });
+    countResult.insertBefore(firstPageButton, countResult.firstChild);
+    const previousPageButton = document.createElement('p');
+    previousPageButton.innerHTML = '<i class="fa-regular fa-chevron-left"></i>';
+    previousPageButton.addEventListener('click', () => {
+      if (currentPage > 1) {
+        currentPage -= 1;
+        renderItems();
+      }
+    });
+    countResult.insertBefore(previousPageButton, countResult.firstChild);
+    const nextPageButton = document.createElement('p');
+    nextPageButton.innerHTML = '<i class="fa-regular fa-chevron-right"></i>';
+    nextPageButton.addEventListener('click', () => {
+      if (currentPage < totalPages) {
+        currentPage += 1;
+        renderItems();
+      }
+    });
+    countResult.appendChild(nextPageButton);
+    const lastPageButton = document.createElement('p');
+    lastPageButton.innerHTML = '<i class="fa-regular fa-chevrons-right"></i>';
+    lastPageButton.addEventListener('click', () => {
+      currentPage = totalPages;
+      renderItems();
+    });
+    countResult.appendChild(lastPageButton);
+    renderItems();
+  }
+
+  renderPagination();
 }
