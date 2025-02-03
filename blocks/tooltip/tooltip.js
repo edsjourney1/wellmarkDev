3a50bdb2c5da388a470ccd7851d9e028a41c0edc
adityakahb <aditya.bhakay@live.com>
2025-02-03 22:02:09 +0530

    WIP tooltips

---
+++ blocks/tooltip/tooltip.js
@@ -0,0 +1,118 @@
+import { computePosition, flip, shift, offset, arrow } from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.6.13/+esm';
+
+const tooltipComponents = Array.from(document.querySelectorAll(`.tooltip`));
+const tooltipMainArr = [];
+
+let areTooltipsCounted = false;
+
+let tooltipsMainArr = [];
+let activeIndex = -1;
+
+export default async function decorate(block) {
+    const thisblock = block;
+    tooltipMainArr.push(thisblock);
+    if (!areTooltipsCounted && tooltipComponents.length === tooltipMainArr.length) {
+        areTooltipsCounted = true;
+        initializeTooltips();
+    }
+};
+
+const updateTooltipPosition = (obj) => {
+  computePosition(obj.link, obj.tooltipEl, {
+    placement: 'top',
+    middleware: [
+        offset(6),
+        flip(),
+        shift({padding: 5}),
+        arrow({element: obj.arrowEl}),
+    ],
+  }).then(({x, y, placement, middlewareData}) => {
+    Object.assign(obj.tooltipEl.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    });
+    
+    const {x: arrowX, y: arrowY} = middlewareData.arrow;
+
+    const staticSide = {
+      top: 'bottom',
+      right: 'left',
+      bottom: 'top',
+      left: 'right',
+    }[placement.split('-')[0]];
+   
+    Object.assign(obj.arrowEl.style, {
+      left: arrowX != null ? `${arrowX}px` : '',
+      top: arrowY != null ? `${arrowY}px` : '',
+      right: '',
+      bottom: '',
+      [staticSide]: '-4px',
+    });
+  });
+};
+
+const showTooltip = (obj) => {
+  obj.tooltipEl.classList.add("tooltip-wrapper-active");
+  activeIndex = obj.index;
+  updateTooltipPosition(obj);
+};
+
+const hideTooltip = (index) => {
+  // console.log("==============index", index);
+  if (tooltipsMainArr[index]) {
+    tooltipsMainArr[index].tooltipEl.classList.remove("tooltip-wrapper-active");
+    activeIndex = -1;
+  }
+  // currentActiveTooltip = null;
+  // currentActiveTooltipArr[index].tooltipEl.classList.remove("tooltip-wrapper-active");
+  // console.log("============currentActiveTooltipArr 1", currentActiveTooltipArr);
+  // currentActiveTooltipArr.splice(index, 1);
+  // console.log("============currentActiveTooltipArr 2", currentActiveTooltipArr);
+};
+
+const initializeTooltips = () => {
+    const allTooltipLinks = Array.from(document.querySelectorAll(`a[href^='#tooltip-']`));
+    const tooltipArr = [];
+    let tooltipNode;
+    allTooltipLinks.forEach((link) => {
+        tooltipNode = tooltipMainArr.find((el) => el.classList.contains(link.getAttribute('href').substring(1)));
+        const tooltipObj = {
+            link,
+            tooltipEl: tooltipNode ? tooltipNode.parentNode : null
+        };
+        tooltipArr.push(tooltipObj);
+    });
+    setTimeout(() => {
+        tooltipArr.forEach((obj, index) => {
+            if (obj.tooltipEl && obj.link) {
+                const genId = `tooltip_${Math.random().toString(16).slice(2)}_${index}`;
+                obj.link.classList.add("tooltip-link");
+                obj.link.setAttribute('aria-describedby', genId);
+                obj.tooltipEl.setAttribute('role', 'tooltip');
+                obj.tooltipEl.setAttribute('id', genId);
+    
+                const arrowEl = document.createElement('div');
+                arrowEl.classList.add('tooltip-arrow');
+                obj.tooltipEl.append(arrowEl);
+                obj.arrowEl = arrowEl;
+                obj.index = index;
+        
+                updateTooltipPosition(obj);
+
+                obj.link.addEventListener('click', (event) => {
+                  event.preventDefault();
+                  showTooltip(obj);
+                });
+            }
+        });
+
+        tooltipsMainArr = tooltipArr;
+
+        document.addEventListener('click', (event) => {
+          let withinBoundaries = event.composedPath().includes(tooltipsMainArr[activeIndex]);
+          if (!withinBoundaries) {
+            hideTooltip(activeIndex);
+          };
+        });
+    }, 100);
+};
