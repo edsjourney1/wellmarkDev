f07557b5075eb9d8475a9a42ffcc141f7b8cc95d
adityakahb <aditya.bhakay@live.com>
2025-02-04 22:14:08 +0530

    Fixed linting issue

--- blocks/video-embed/video-embed.js
+++ blocks/video-embed/video-embed.js
@@ -17,12 +17,12 @@ const loadScript = (url, callback, type) => {
 };
 
 const getDefaultEmbed = (
-  url
+  url,
 ) => `<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;'>
-      <iframe src='${url.href}' style='border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;' allowfullscreen=''
-        scrolling='no' allow='encrypted-media' title='Content from ${url.hostname}' loading='lazy'>
-      </iframe>
-    </div>`;
+  <iframe src='${url.href}' style='border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;' allowfullscreen=''
+    scrolling='no' allow='encrypted-media' title='Content from ${url.hostname}' loading='lazy'>
+  </iframe>
+</div>`;
 
 const embedYoutube = (url, autoplay) => {
   const usp = new URLSearchParams(url.search);
@@ -33,11 +33,11 @@ const embedYoutube = (url, autoplay) => {
     [, vid] = url.pathname.split('/');
   }
   const embedHTML = `<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;'>
-        <iframe src='https://www.youtube.com${
-          vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed
-        }' style='border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;' 
-        allow='autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture' allowfullscreen='' scrolling='no' title='Content from Youtube' loading='lazy'></iframe>
-      </div>`;
+    <iframe src='https://www.youtube.com${
+      vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed
+    }' style='border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;' 
+    allow='autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture' allowfullscreen='' scrolling='no' title='Content from Youtube' loading='lazy'></iframe>
+  </div>`;
   return embedHTML;
 };
 
@@ -45,11 +45,11 @@ const embedVimeo = (url, autoplay) => {
   const [, video] = url.pathname.split('/');
   const suffix = autoplay ? '?muted=1&autoplay=1' : '';
   const embedHTML = `<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;'>
-        <iframe src='https://player.vimeo.com/video/${video}${suffix}' 
-        style='border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;' 
-        frameborder='0' allow='autoplay; fullscreen; picture-in-picture' allowfullscreen  
-        title='Content from Vimeo' loading='lazy'></iframe>
-      </div>`;
+  <iframe src='https://player.vimeo.com/video/${video}${suffix}' 
+    style='border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;' 
+    frameborder='0' allow='autoplay; fullscreen; picture-in-picture' allowfullscreen  
+    title='Content from Vimeo' loading='lazy'></iframe>
+  </div>`;
   return embedHTML;
 };
 
@@ -79,9 +79,7 @@ const loadEmbed = (block, link, autoplay) => {
     },
   ];
 
-  const config = EMBEDS_CONFIG.find((e) =>
-    e.match.some((match) => link.includes(match))
-  );
+  const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
   const url = new URL(link);
   if (config) {
     block.innerHTML = config.embed(url, autoplay);
@@ -101,8 +99,7 @@ export default function decorate(block) {
   if (placeholder) {
     const wrapper = document.createElement('div');
     wrapper.className = 'video-embed-placeholder';
-    wrapper.innerHTML =
-      '<div class="video-embed-placeholder-play"><button type="button" title="Play"></button></div>';
+    wrapper.innerHTML = '<div class="video-embed-placeholder-play"><button type="button" title="Play"></button></div>';
     wrapper.prepend(placeholder);
     wrapper.addEventListener('click', () => {
       loadEmbed(block, link, true);
