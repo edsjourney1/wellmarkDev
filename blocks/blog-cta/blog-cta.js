4599126970a6245c9c29cae8d770e8e59aebf1e9
adityakahb <aditya.bhakay@live.com>
2025-02-06 12:46:14 +0530

    Fixed linting issue

--- blocks/blog-cta/blog-cta.js
+++ blocks/blog-cta/blog-cta.js
@@ -2,6 +2,6 @@ export default async function decorate(block) {
   const thisBlock = block;
   const [blogCtaBody, blogCtaBtn] = Array.from(thisBlock.children);
 
-  blogCtaBtn?.classList.add("blog-cta-btn");
-  blogCtaBody?.classList.add("blog-cta-body");
+  blogCtaBtn?.classList.add('blog-cta-btn');
+  blogCtaBody?.classList.add('blog-cta-body');
 }
