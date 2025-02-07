b085c7abf72b1bfeea917d2bcf0f0a90101b6d5f
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2025-02-07 10:44:34 +0530

    blog-hero-article new changes

--- blocks/blog-hero-article/blog-hero-article.js
+++ blocks/blog-hero-article/blog-hero-article.js
@@ -3,7 +3,10 @@ import myJson from '../../scripts/constants.js';
 export default async function decorate(block) {
   const mainPageURL = block.textContent;
   console.log(mainPageURL);
-  const postArticle = myJson.find((item) => item.url === mainPageURL);
+  const postArticle = myJson.find((item) => {
+    console.log(item.url);
+    return item.url === mainPageURL;
+  });
   console.log(postArticle);
   const blogHero = document.createElement('div');
   const imgDiv = document.createElement('div');
