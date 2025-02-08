f88f5db0a0501e4e815e08b4f320929378a778b7
adityakahb <aditya.bhakay@live.com>
2025-02-08 22:21:56 +0530

    Siteheader WIP

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,100 +1,169 @@
-import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
+import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
-export default async function decorate(block) {
-    
-    const thisBlock = block;
-    const siteHeaderMeta = getMetadata('/header-fragments/siteheader-fragment');
-    const siteHeaderPath = siteHeaderMeta ? new URL(siteHeaderMeta, window.location).pathname : '/header-fragments/siteheader-fragment';
+const formMainNavigation = (thisBlock, navUl, navCtaEl) => {
 
-    const fragment = await loadFragment(siteHeaderPath);
-    const headerFragment = fragment.querySelector('.siteheader-container');
+  console.log('============thisBlock', thisBlock);
+  const mobileBtnWrapperEl = document.createElement('div');
+  mobileBtnWrapperEl.className = 'siteheader-mobile-wrapper';
+  mobileBtnWrapperEl.innerHTML = `<button type='button'>
+          <div>${navCtaEl.querySelector('p:first-child').innerHTML}</div>
+          <div>${navCtaEl.querySelector('p:last-child').innerHTML}</div>
+      </button>`;
+  thisBlock.append(mobileBtnWrapperEl);
 
-    if (headerFragment) {
-        const [logoInfo, searchInfo, searchToggle, loginInfo, logoutInfo, mobileInfo] = Array.from(headerFragment.querySelector('.siteheader > div').children);
+  const navEl = document.createElement('nav');
+  navEl.append(navUl);
+  thisBlock.append(navEl);
+  // const loginWrapEl = thisBlock.querySelector('.siteheader-login-wrapper-cta');
+  // const loginCtaEl = loginWrapEl?.querySelector('button');
 
-        const navMaskEl = document.createElement('div');
-        navMaskEl.className = 'siteheader-nav-mask';
+  // loginCtaEl?.addEventListener('click', () => {
+  //     if (loginWrapEl.classList.contains('siteheader-login-wrapper-cta-active')) {
+  //         loginWrapEl.classList.remove('siteheader-login-wrapper-cta-active');
+  //     } else {
+  //         loginWrapEl.classList.add('siteheader-login-wrapper-cta-active');
+  //     }
+  // });
 
-        let logoWrapperStr = '';
-        if (logoInfo) {
-            logoWrapperStr = `<div class='siteheader-logo-wrapper'>
+  // console.log("==============", thisBlock, navUl);
+  // console.log('===========navUl', navUl);
+  // const navArr = [];
+  // const l0li = Array.from(navUl.children);
+  // l0li.forEach(l0liEl => {
+  //   const l0liObj = {
+  //     l0: l0liEl.querySelector('a')
+  //   };
+  //   if (l0liEl.querySelector('ul')) {
+  //     const l1li = Array.from(l0liEl.querySelector('ul').children);
+  //     console.log('===========l1li', l1li);
+  //   }
+  //   navArr.push(l0liObj);
+  // });
+
+  // console.log('===============', navArr);
+};
+
+const formMainHeader = (thisBlock, headerFragment) => {
+  const headerWrapper = thisBlock.closest('.header-wrapper');
+
+  if (headerFragment) {
+    const [
+      logoInfo,
+      searchInfo,
+      searchToggle,
+      loginInfo,
+      logoutInfo,
+    ] = Array.from(headerFragment.querySelector('.siteheader > div:first-child').children);
+
+    const [navCtaEl] = Array.from(headerFragment.querySelector('.siteheader > div:nth-child(2)').children);
+
+    const [navUlEl] = Array.from(headerFragment.querySelector('.siteheader > div:last-child').children);
+    const navUl = navUlEl?.querySelector('ul');
+
+    const navMaskEl = document.createElement('div');
+    navMaskEl.className = 'siteheader-mask';
+
+    const paddingEl = document.createElement('div');
+    paddingEl.className = 'header-padding';
+
+    let logoWrapperStr = '';
+    if (logoInfo) {
+      logoWrapperStr = `<div class='siteheader-logo-wrapper'>
                 <a href='${logoInfo.querySelector('a').getAttribute('href')}'>
-                    <img src='${logoInfo.querySelector('a').innerHTML}' alt='Wellmark Logo'>
+                    <img src='${
+                      logoInfo.querySelector('a').innerHTML
+                    }' alt='Wellmark Logo'>
                 </a>
             </div>`;
-        }
+    }
 
-        const searchSectionStartStr = `<div class='siteheader-right-section'>`;
-        const searchSectionEndStr = `</div>`;
+    const searchSectionStartStr = `<div class='siteheader-right-section'>`;
+    const searchSectionEndStr = `</div>`;
 
-        let searchWrapperStr = '';
-        if (searchInfo && searchToggle) {
-            searchWrapperStr = `<div class='siteheader-search-wrapper'>
-            <button type='button' aria-label='Toggle Search'>${searchToggle.querySelector('p').innerHTML}</button>
+    let searchWrapperStr = '';
+    if (searchInfo && searchToggle) {
+      searchWrapperStr = `<div class='siteheader-search-wrapper'>
+            <button type='button' aria-label='Toggle Search'>${
+              searchToggle.querySelector('p:first-child').innerHTML
+            }
+                ${searchToggle.querySelector('p:last-child').innerHTML}</button>
             <div class='siteheader-search-content'>
                     <form action='/' id='siteheader-search-form'>
                         <div class='siteheader-search-input-wrapper'>
-                            <label for='siteheader-search-input'>${searchInfo.querySelector('p:first-child em').innerHTML}</label>
-                            <input type='search' id='siteheader-search-input' placeholder='${searchInfo.querySelector('p:nth-child(2) em').innerHTML}'/>
-                            <button type='submit' aria-label='Search'>${searchInfo.querySelector('p:last-child em').innerHTML}</button>
+                            <label for='siteheader-search-input'>${
+                              searchInfo.querySelector('p:first-child em')
+                                .innerHTML
+                            }</label>
+                            <input type='search' id='siteheader-search-input' placeholder='${
+                              searchInfo.querySelector('p:nth-child(2) em')
+                                .innerHTML
+                            }'/>
+                            <button type='submit' aria-label='Search'>${
+                              searchInfo.querySelector('p:last-child em')
+                                .innerHTML
+                            }</button>
                         </div>
                     </form>
                 </div>
             </div>`;
-        }
-        
-        let loginWrapperStr = '';
-        if (loginInfo) {
-            loginWrapperStr = `<div class='siteheader-login-wrapper'>
+    }
+
+    let loginWrapperStr = '';
+    if (loginInfo) {
+      loginWrapperStr = `<div class='siteheader-login-wrapper'>
                 <div class='siteheader-login-wrapper-cta'>
                     <button type='button'>
                         <span>${loginInfo.querySelector('p').innerHTML}</span>
                     </button>
                 </div>
             </div>`;
-        }
+    }
 
-        let logoutWrapperStr = '';
-        if (logoutInfo) {
-            logoutWrapperStr = `<div class='siteheader-logout-wrapper siteheader-logout-wrapper-hidden'>
+    let logoutWrapperStr = '';
+    if (logoutInfo) {
+      logoutWrapperStr = `<div class='siteheader-logout-wrapper siteheader-logout-wrapper-hidden'>
                 <button type='button'>
                     <span>${logoutInfo.querySelector('p').innerHTML}</span>
                 </button>
             </div>`;
-        }
-
-        let mobileBtnStr = '';
-        if (mobileInfo) {
-            mobileBtnStr = `<div class='siteheader-mobile-wrapper'>
-                <button type='button'>
-                    <div class='siteheader-mobile-toggle-icon'></div>
-                    <span>${mobileInfo.children[0].innerHTML}</span>
-                    <span>${mobileInfo.children[1].innerHTML}</span>
-                </button>
-            </div>`;
-        }
-
-        headerFragment.innerHTML = logoWrapperStr
-            + searchSectionStartStr
-            + searchWrapperStr
-            + loginWrapperStr
-            + logoutWrapperStr
-            + searchSectionEndStr
-            + mobileBtnStr;
-        
-        thisBlock.append(headerFragment);
-        thisBlock.closest('.header-wrapper').append(navMaskEl);
-
-        const loginWrapEl = thisBlock.querySelector('.siteheader-login-wrapper-cta');
-        const loginCtaEl = loginWrapEl?.querySelector('button');
-
-        loginCtaEl?.addEventListener('click', () => {
-            if (loginWrapEl.classList.contains('siteheader-login-wrapper-cta-active')) {
-                loginWrapEl.classList.remove('siteheader-login-wrapper-cta-active');
-            } else {
-                loginWrapEl.classList.add('siteheader-login-wrapper-cta-active');
-            }
-        });
     }
+
+    headerFragment.innerHTML =
+      '<div class="siteheader-outer"><div class="siteheader-inner">' + 
+      logoWrapperStr +
+      searchSectionStartStr +
+      searchWrapperStr +
+      loginWrapperStr +
+      logoutWrapperStr +
+      searchSectionEndStr +
+      '</div></div>';
+
+    thisBlock.append(headerFragment);
+
+    if (headerWrapper) {
+      headerWrapper.parentNode.insertBefore(navMaskEl, headerWrapper);
+      headerWrapper.parentNode.insertBefore(paddingEl, headerWrapper);
+    }
+
+    if (navUl && navCtaEl) {
+      formMainNavigation(thisBlock, navUl, navCtaEl);
+    }
+  }
+};
+
+export default async function decorate(block) {
+  const thisBlock = block;
+  const siteHeaderMeta = getMetadata('/header-fragments/siteheader-fragment');
+  const siteHeaderPath = siteHeaderMeta
+    ? new URL(siteHeaderMeta, window.location).pathname
+    : '/header-fragments/siteheader-fragment';
+
+  const fragment = await loadFragment(siteHeaderPath);
+
+  const headerFragment = fragment.querySelector('.siteheader-container');
+
+  if (headerFragment) {
+    formMainHeader(thisBlock, headerFragment);
+  }
 }
