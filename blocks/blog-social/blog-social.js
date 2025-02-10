b231a1f36fc44a2e808e8032bff4b26ea87d60c7
G <2035448@cognizant.com>
2025-02-10 11:08:32 +0530

    social link testing

--- blocks/blog-social/blog-social.js
+++ blocks/blog-social/blog-social.js
@@ -19,14 +19,33 @@ export default function decorate(block) {
     shareIcon.classList.add('share-icons');
     const fb = document.createElement('span');
     fb.innerHTML = '<i class="fa-brands fa-facebook-f"></i>';
+    fb.addEventListener('click', () => {
+      const url = encodeURIComponent(window.location.href);
+      window.open(`https://www.facebook.com/sharer/sharer.php?t=${document.title}&u=${url}`, '_blank');
+    });
     const twitter = document.createElement('span');
     twitter.innerHTML = '<i class="fa-brands fa-twitter"></i>';
+    twitter.addEventListener('click', () => {
+      const twitterlink = encodeURIComponent(window.location.href);
+      window.open(`https://twitter.com/intent/tweet?url=${twitterlink}&text=${document.title}`, '_blank');
+    });
     const linked = document.createElement('span');
     linked.innerHTML = '<i class="fa-brands fa-linkedin-in"></i>';
+    linked.addEventListener('click', () => {
+      const inlink = encodeURIComponent(window.location.href);
+      window.open(`https://www.linkedin.com/sharing/share-offsite/?url=${inlink}&text=${document.title}`, '_blank');
+    });
     const mail = document.createElement('span');
     mail.innerHTML = '<i class="fa-solid fa-envelope"></i>';
+    mail.addEventListener('click', () => {
+      const maillink = encodeURIComponent(window.location.href);
+      window.open(`mailto:?subject=${document.title}&body=${maillink}`, '_blank');
+    });
     const print = document.createElement('span');
     print.innerHTML = '<i class="fa-solid fa-print"></i>';
+    print.addEventListener('click', () => {
+      window.print();
+    });
     shareIcon.append(fb, twitter, linked, mail, print);
     share.append(shareIcon);
   }
