11006a8b6620c3246490ec2ac89c73d551fba559
Muthu Soundara Pandian <soundhar.sms1992@gmail.com>
2025-02-11 23:58:45 +0530

    lint-fix

--- blocks/blog-article-information/blog-article-information.js
+++ blocks/blog-article-information/blog-article-information.js
@@ -1,70 +1,3 @@
-// export default function decorate(block) {
-//   const blockChild = block.children[0].children[0];
-//   const dates = block.children[0].children[0].querySelector('p');
-//   if (dates) {
-//     dates.parentElement.classList.add('date');
-//   }
-//   setTimeout(() => {
-//     // Check if block and its children exist
-//     if (block && block.children && block.children[0] && block.children[0].children[1]) {
-//       const date = block.children[0].children[1].querySelector('p');
-//       // Check if date element exists
-//       if (date) {
-//         const modifiedDate = window.BlogLastModified;
-//         // Check if modifiedDate exists
-//         if (modifiedDate) {
-//           const spanmodified = document.createElement('span');
-//           spanmodified.innerHTML = modifiedDate;
-//           date.append(spanmodified);
-//           // Check if blockChild exists before appending
-//           if (blockChild) {
-//             blockChild.append(date);
-//           }
-//         } else {
-//           const spanNull = document.createElement('span');
-//           spanNull.innerHTML = 'MM/DD/YYYY';
-//           date.append(spanNull);
-//           // Check if blockChild exists before appending
-//           if (blockChild) {
-//             blockChild.append(date);
-//           }
-//         }
-//       }
-//     }
-//   }, 500);
-//   const author = block.children[0].children[2].querySelector('p');
-//   if (author) {
-//     author.parentElement.classList.add('author');
-//   }
-//   const authorPtag = document.querySelector('.author p');
-//   const authorName = document.querySelector('meta[name="author-name"]').content;
-//   const authorSpantag = document.createElement('p');
-//   const nameofAuthor = document.createTextNode(authorName);
-//   if (authorPtag) {
-//     authorSpantag.appendChild(nameofAuthor);
-//     authorPtag.appendChild(authorSpantag);
-//   }
-//   const linkPara = block.children[1].children[0].querySelector('p');
-//   if (linkPara) {
-//     linkPara.parentElement.classList.add('article-link');
-//     const links = linkPara.querySelectorAll('a');
-//     links.forEach((link) => {
-//       if (link.href || link.textContent.trim()) {
-//         const anchorPtag = document.createElement('p');
-//         anchorPtag.append(link);
-//         linkPara.parentElement.append(anchorPtag);
-//       } else {
-//         // Remove the empty <a> tag
-//         link.remove();
-//       }
-//     });
-//   }
-//   const audio = block.children[2].children[0].querySelector('p');
-//   if (audio) {
-//     audio.parentElement.classList.add('article-audio');
-//   }
-// }
-
 export default function decorate(block) {
   const audio = block.children[0].children[0].querySelector('p');
   if (audio) {
@@ -125,9 +58,9 @@ export default function decorate(block) {
   authorPtag.prepend(span);
   author.append(authorPtag);
   dates.append(date, author);
-  // create a new div for articles category 
+  // create a new div for articles category
   const categoryValue = document.createElement('div');
-  // create a new div for articles 
+  // create a new div for articles
   const articles = document.createElement('div');
   articles.classList.add('article-link');
   // create a p tag for reading timez
@@ -146,11 +79,11 @@ export default function decorate(block) {
   });
   const readingTime = document.querySelector('meta[name="read-time"]').content;
   if (readingTime) {
-    const span = document.createElement('span');
-    span.innerHTML = readingTime;
+    const spanTime = document.createElement('span');
+    spanTime.innerHTML = readingTime;
     const spanSec = document.createElement('span');
     spanSec.innerHTML = 'Min Read';
-    readTime.append(span, spanSec);
+    readTime.append(spanTime, spanSec);
   }
   articles.append(readTime, articleLinks);
   categoryValue.appendChild(articles);
