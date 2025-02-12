f54a86597756c97c5eaccc387330d927b7f60e84
edsjourney1 <edsjourney1@gmail.com>
2025-02-12 17:43:12 +0530

    accessibility mode

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,8 @@ import {
   loadPublishedDate,
 } from './aem.js';
 import { decorateExternalImages } from './externalImage.js';
-
+import initAccessibilityMode from '../tools/sidekick/plugins/accessibility-mode/accessibility-mode.js';
+let isA11yModeActive = false;
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
@@ -31,6 +32,148 @@ function buildHeroBlock(main) {
     main.prepend(section);
   }
 }
+
+/** 
+ * create an element.
+
+ * @param {string} tagName the tag for the element
+
+ * @param {object} props properties to apply
+
+ * @param {string|Element} html content to add
+
+ * @returns the element
+
+ */
+
+export function createElement(tagName, props, html) {
+
+  const elem = document.createElement(tagName);
+
+  if (props) {
+
+    Object.keys(props).forEach((propName) => {
+
+      const val = props[propName];
+
+      if (propName === 'class') {
+
+        const classesArr = (typeof val === 'string') ? [val] : val;
+
+        elem.classList.add(...classesArr);
+
+      } else {
+
+        elem.setAttribute(propName, val);
+
+      }
+
+    });
+
+  }
+
+
+
+  if (html) {
+
+    const appendEl = (el) => {
+
+      if (el instanceof HTMLElement || el instanceof SVGElement) {
+
+        elem.append(el);
+
+      } else {
+
+        elem.insertAdjacentHTML('beforeend', el);
+
+      }
+
+    };
+
+
+
+    if (Array.isArray(html)) {
+
+      html.forEach(appendEl);
+
+    } else {
+
+      appendEl(html);
+
+    }
+
+  }
+
+
+
+  return elem;
+
+}
+
+
+
+const accessibilityMode = async (e) => {
+
+  const pluginButton = e.target.shadowRoot.querySelector('plugin-action-bar')
+
+    ? e.target.shadowRoot.querySelector('plugin-action-bar').shadowRoot.querySelector('.accessibility-mode')
+
+    : e.target.shadowRoot.querySelector('.accessibility-mode > button');
+
+
+
+  isA11yModeActive = !isA11yModeActive;
+
+
+
+  if (isA11yModeActive) {
+
+    pluginButton.style.backgroundColor = '#4e9a17';
+
+    pluginButton.style.color = '#fff';
+
+  } else {
+
+    pluginButton.removeAttribute('style');
+
+  }
+
+
+
+  document.querySelector('body').classList.toggle('accessibility-mode-active');
+
+  await initAccessibilityMode(isA11yModeActive);
+
+};
+
+
+
+const sk = document.querySelector('aem-sidekick') || document.querySelector('helix-sidekick');
+
+
+
+if (sk) {
+
+  sk.addEventListener('custom:accessibility-mode', accessibilityMode);
+
+} else {
+
+  document.addEventListener('sidekick-ready', () => {
+
+    const sk = document.querySelector('aem-sidekick') || document.querySelector('helix-sidekick');
+
+    sk.addEventListener('custom:accessibility-mode', accessibilityMode);
+
+  }, {
+
+    once: true,
+
+  });
+
+}
+
+
+
 /**
  * load fonts.css and set a session storage flag
  */
