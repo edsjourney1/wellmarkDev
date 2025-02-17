340e250586278ade1f449af1d12e5835cb3c08e0
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2025-02-17 13:08:19 +0530

    promo-lg-icon defect

--- blocks/promo-text-lg-icon/promo-text-lg-icon.js
+++ blocks/promo-text-lg-icon/promo-text-lg-icon.js
@@ -7,13 +7,14 @@ export default function decorate(block) {
   const div2 = document.createElement('div');
   div2.classList.add('heading-icon-div');
   const svg = svgIcon;
+  svg.classList.add('svg-icon');
   const heading = document.createElement('h2');
   heading.append(title);
   heading.classList.add('title');
+  div2.append(svg, heading);
   if (block.children[3] === '' || block.children[3] === undefined) {
-    div2.append(heading);
-  } else {
-    div2.append(svg, heading);
+    const iconCheck = document.querySelector('.svg-icon');
+    iconCheck.remove();
   }
   const paragraph = document.createElement('p');
   paragraph.classList.add('description');
