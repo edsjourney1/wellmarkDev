4353b7e1e1fac726f742d5fb2825c715ac65caee
Muthu Soundara Pandian <soundhar.sms1992@gmail.com>
2025-02-17 18:23:17 +0530

    pull-quote updates

--- blocks/pull-quote/pull-quote.js
+++ blocks/pull-quote/pull-quote.js
@@ -4,6 +4,10 @@ export default function decorate(block) {
   // get the first child
   const quotecontent = block.children[0].children[0];
   quotecontent.classList.add('quote-content');
+  const content = quotecontent.children[0];
+  if (content) {
+    content.classList.add('content');
+  }
   // create div for top-icon
   const icontopDiv = document.createElement('div');
   icontopDiv.classList.add('icon-top');
@@ -18,4 +22,18 @@ export default function decorate(block) {
   iconbtmSpan.innerHTML = '<i class="fa-solid fa-quote-left"></i>';
   iconbtmDiv.appendChild(iconbtmSpan);
   firstChild.appendChild(iconbtmDiv);
+  // create a div for Author
+  const authorName = block.children[1].children[0].children[0];
+  if (authorName) {
+    authorName.classList.add('authorname');
+    quotecontent.appendChild(authorName);
+  }
+  const authorPosition = block.children[1].children[1].children[0];
+  if (authorPosition) {
+    authorPosition.classList.add('authorposition');
+    quotecontent.appendChild(authorPosition);
+  }
+  // Remove empty divs
+  const emptyDivs = block.querySelectorAll('div:empty');
+  emptyDivs.forEach((div) => div.remove());
 }
