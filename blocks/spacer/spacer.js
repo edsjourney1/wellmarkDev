c591681640e98f5d953630b26af4404fa9136f91
adityakahb <aditya.bhakay@live.com>
2025-02-18 17:30:26 +0530

    Search page WIP and spacer component

--- blocks/spacer/spacer.js
+++ blocks/spacer/spacer.js
@@ -1,7 +1,6 @@
 export default async function decorate(block) {
   const thisBlock = block;
   const pTag = thisBlock.querySelector('p');
-  console.log('==============thisblock', pTag.innerHTML);
   if (pTag) {
     thisBlock.style.height = `${pTag.innerHTML}px`;
   }
