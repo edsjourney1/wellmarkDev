bff78cd81d75854c4dc2958afe0d323641f71a2c
adityakahb <aditya.bhakay@live.com>
2025-02-19 20:57:44 +0530

    Search page WIP

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -1,5 +1,7 @@
+
 export default async function decorate(block) {
   const thisBlock = block;
+  console.log('============thisBlock', thisBlock);
   thisBlock.classList.add('search-hidden');
   thisBlock.setAttribute('aria-hidden', true);
 }
