d2987eaea2a194995c9eddbd164220962ef99ce0
Muthu Soundara Pandian <soundhar.sms1992@gmail.com>
2025-02-19 23:35:11 +0530

    article-information fixes

--- blocks/blog-article-information/blog-article-information.js
+++ blocks/blog-article-information/blog-article-information.js
@@ -7,19 +7,19 @@ export default function decorate(block) {
   // create a div for the date section
   const date = document.createElement('div');
   date.classList.add('date');
-  // create a p tag for published dates
-  const publish = document.createElement('p');
-  const publishSpan = document.createElement('span');
-  publishSpan.innerText = 'Published:';
   // create span tag for last published date
   const publishedDate = document.querySelector('meta[name="publish-date"]');
   if (publishedDate) {
+    // create a p tag for published dates
+    const publish = document.createElement('p');
+    const publishSpan = document.createElement('span');
+    publishSpan.innerText = 'Published:';
     const publishdata = publishedDate.content;
     const dateformat = document.createElement('span');
     dateformat.innerText = publishdata;
     publish.append(publishSpan, dateformat);
+    date.prepend(publish);
   }
-  date.prepend(publish);
   // create a p tag for modified dates
   const modify = document.createElement('p');
   const modifyspan = document.createElement('span');
@@ -28,15 +28,22 @@ export default function decorate(block) {
   // function for last-modified date append dynamically
   setTimeout(() => {
     const queryModify = block.querySelector('.date');
-    const updated = queryModify.children[1];
-    // Check if date element exists
-    if (updated) {
-      const modifiedDate = window.BlogLastModified;
-      // Check if modifiedDate exists
-      if (modifiedDate) {
-        const spanmodified = document.createElement('span');
-        spanmodified.innerHTML = modifiedDate;
-        updated.append(spanmodified);
+    console.log(queryModify);
+    if (queryModify) {
+      const publishedElement = Array.from(queryModify.children).find((child) => child.innerText === 'Last Updated:');
+      const published = publishedElement ? publishedElement.innerText : null;
+      const updated = published;
+      // Check if date element exists
+      if (updated) {
+        const modifiedDate = window.BlogLastModified;
+        console.log(modifiedDate, 'hello-1');
+        // Check if modifiedDate exists
+        if (modifiedDate) {
+          const spanmodified = document.createElement('span');
+          spanmodified.innerHTML = modifiedDate;
+          console.log(modifiedDate, 'hello');
+          publishedElement.append(spanmodified);
+        }
       }
     }
   }, 500);
@@ -44,10 +51,11 @@ export default function decorate(block) {
   // create a div for author
   const author = document.createElement('div');
   author.classList.add('author');
-  const authorName = document.querySelector('meta[name="author"]').content;
+  const authorName = document.querySelector('meta[name="author"]');
   if (authorName) {
+    const authorContent = authorName.content;
     const authorPtag = document.createElement('p');
-    authorPtag.innerText = authorName;
+    authorPtag.innerText = authorContent;
     // create a span tag for the icon
     const span = document.createElement('span');
     span.classList.add('icon', 'icon-regular--user');
@@ -66,18 +74,24 @@ export default function decorate(block) {
   articles.classList.add('article-link');
   // create a p tag for reading timez
   const readTime = document.createElement('p');
-  // create a p tag for article links
-  const articleLinks = document.createElement('p');
   // create a tag for the links
   const categoryMetaTag = document.querySelector('meta[name="category"]');
-  const categoryValues = categoryMetaTag.content.split(', ');
-  categoryValues.forEach((category) => {
-    // Create a new span element
-    const aTag = document.createElement('a');
-    aTag.textContent = category;
-    // Append the span element to the container
-    articleLinks.appendChild(aTag);
-  });
+  if (categoryMetaTag) {
+    const categoryValues = categoryMetaTag.content;
+    if (categoryValues) {
+      // create a p tag for article links
+      const articleLinks = document.createElement('p');
+      const splitValues = categoryValues.split(', ');
+      splitValues.forEach((category) => {
+        // Create a new span element
+        const aTag = document.createElement('a');
+        aTag.textContent = category;
+        // Append the span element to the container
+        articleLinks.appendChild(aTag);
+        articles.append(articleLinks);
+      });
+    }
+  }
   const readingTime = document.querySelector('meta[name="readingtime"]');
   if (readingTime) {
     const timeData = readingTime.content;
@@ -87,7 +101,7 @@ export default function decorate(block) {
     spanSec.innerHTML = 'Min Read';
     readTime.append(spanTime, spanSec);
   }
-  articles.append(readTime, articleLinks);
+  articles.prepend(readTime);
   categoryValue.appendChild(articles);
   block.prepend(dates, categoryValue);
 }
