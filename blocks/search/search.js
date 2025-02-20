c3e7cf35bc94e4495d5460038b8f6427767ea5a3
adityakahb <aditya.bhakay@live.com>
2025-02-20 10:00:32 +0530

    Search page WIP

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -3,9 +3,31 @@ const searchParent = document.querySelector('.search-container');
 const allSearchItems = Array.from(document.querySelectorAll('.search-wrapper'));
 const allSearchBlocks = [];
 
+const buildSearchBanner = (bannerBlock, bannerEl, searchGrid) => {
+
+  console.log('===========bannerBlock', bannerBlock);
+  const [bannerH1, bannerP] = Array.from(bannerBlock.children);
+
+  bannerEl.append(bannerH1.querySelector(':scope > div'));
+  searchGrid.append(bannerEl);
+};
+
 const initSearch = () => {
-  console.log('==============sampleData', sampleData);
-  const searchGrid = document.querySelector('section');
+  const searchWrap = document.createElement('section');
+  const searchGrid = document.createElement('div');
+  const searchBanner = document.createElement('div');
+  const searchLeftCol = document.createElement('div');
+  const searchRightCol = document.createElement('div');
+  searchWrap.classList.add('search-grid-wrapper');
+  searchGrid.classList.add('search-grid');
+  // searchBanner.innerHTML = '<div class="search-banner-section"></div>';
+
+  buildSearchBanner(allSearchBlocks[0], searchBanner, searchGrid);
+
+  searchGrid.append(searchLeftCol);
+  searchGrid.append(searchRightCol);
+  searchWrap.append(searchGrid);
+  searchParent.append(searchWrap);
 };
 
 export default async function decorate(block) {
