b85ee77b729f50ef26b322bfc01342f1ba174c52
adityakahb <aditya.bhakay@live.com>
2025-02-22 09:03:30 +0530

    Search page WIP

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -3,36 +3,69 @@ import sampleData from './sample-data.js';
 const searchParent = document.querySelector('.search-container');
 const allSearchItems = Array.from(document.querySelectorAll('.search-wrapper'));
 const allSearchBlocks = [];
+let searchGrid;
+let searchLeftCol;
+let searchRightCol;
 
-const buildSearchBanner = () => { // bannerBlock, bannerEl, searchGrid
-  console.log('===========bannerBlock', sampleData);
-  // const [bannerH1, bannerP] = Array.from(bannerBlock.children);
+const buildSearchGrid = () => {
+  if (searchGrid && searchLeftCol && searchRightCol) {
+    const searchList = document.createElement('ul');
+    let str = '';
+    (sampleData.data || []).forEach((item) => {
+      str += `<li>
+        <a href='${item.link}'><h3>${item.title}</h3></a>
+      </li>`;
+    });
+    searchRightCol.innerHTML = str;
+    searchRightCol.append(searchList);
+  }
+};
 
-  // bannerEl.append(bannerH1.querySelector(':scope > div'));
-  // searchGrid.append(bannerEl);
+const buildSearchBanner = (bannerBlock) => { // bannerBlock, bannerEl, searchGrid
+  const searchFormParent = document.createElement('div');
+  const searchLabelWrapper = bannerBlock.querySelector(':scope > div:last-child');
+  if (searchLabelWrapper) {
+    const [searchLabel, ctaLabel] = searchLabelWrapper.children;
+    searchFormParent.innerHTML = `<form><div class='search-form-grid'>
+      <div class='search-form-col'>
+        <label for='search_term'>${searchLabel.querySelector('p').innerHTML}</label>
+        <input id='search_term' placeholder='${searchLabel.querySelector('p').innerHTML}' type='search'/>
+      </div>
+      <div class='search-form-col'>
+        <button type='submit'>${ctaLabel.querySelector('p').innerHTML}</button>
+      </div>
+    </div></form>`;
+    bannerBlock.append(searchFormParent);
+  }
 };
 
 const initSearch = () => {
   const searchWrap = document.createElement('section');
-  const searchGrid = document.createElement('div');
-  const searchLeftCol = document.createElement('div');
-  const searchRightCol = document.createElement('div');
+  searchGrid = document.createElement('div');
+  searchLeftCol = document.createElement('div');
+  searchRightCol = document.createElement('div');
   searchWrap.classList.add('search-grid-wrapper');
   searchGrid.classList.add('search-grid');
-  // searchBanner.innerHTML = '<div class="search-banner-section"></div>';
 
   buildSearchBanner(allSearchBlocks[0], searchGrid);
 
   searchGrid.append(searchLeftCol);
   searchGrid.append(searchRightCol);
   searchWrap.append(searchGrid);
-  searchParent.append(searchWrap);
+  searchParent.parentElement.append(searchWrap);
+
+  buildSearchGrid();
 };
 
 export default async function decorate(block) {
   const thisBlock = block;
-  // thisBlock.classList.add('search-hidden');
-  // thisBlock.setAttribute('aria-hidden', true);
+  if (!thisBlock.classList.contains('search-banner')) {
+    thisBlock.classList.add('search-hidden');
+    thisBlock.setAttribute('aria-hidden', true);
+  } else {
+    thisBlock.querySelector(':scope > div:last-child')?.classList.add('search-hidden');
+    thisBlock.querySelector(':scope > div:last-child')?.setAttribute('aria-hidden', true);
+  }
   allSearchBlocks.push(thisBlock);
 
   if (allSearchItems.length === allSearchBlocks.length) {
