dc3c5574e80263253d0f8f0259bcf749560d4b72
Nath <2078150@cognizant.com>
2025-02-25 10:29:05 +0530

    new icon blocks and blog-cta bug fix

---
+++ blocks/icons/icons.js
@@ -0,0 +1,35 @@
+export default async function decorate(block) {
+  const iconblock = document.querySelector(".icons");
+
+  [...block.children].forEach((row) => {
+    row.classList.add("row");
+    [...row.children].forEach((col) => {
+      const para = col.querySelector("p");
+      const codeelm = document.createElement("button");
+      codeelm.classList.add("copy");
+      const spn = col.querySelector("span");
+      const classes = Array.from(spn.classList);
+      //replace the string 'icon' at begining with ':' and add ':' at end
+      const code = classes[1].replace(/^icon-/, ":").concat(":");
+      codeelm.textContent = code;
+      para.appendChild(codeelm);
+    });
+  });
+
+  let cpybtn = document.querySelectorAll(".copy");
+
+  cpybtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const text = this.textContent;
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          console.log(text);
+          alert("copied");
+        })
+        .catch((e) => {
+          alert("cant copy");
+        });
+    });
+  });
+}
